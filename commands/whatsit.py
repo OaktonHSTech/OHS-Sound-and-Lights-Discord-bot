66b872daf9159fce24ba3b2f28c8787fa14bf7bd
OaktonHSTech <oaktonhstech@gmail.com>
2025-07-20 09:40:26 -0400

    added ping to whatsit

--- commands/whatsit.py
+++ commands/whatsit.py
@@ -5,6 +5,7 @@ async def setup_whatsit_command(bot):
             "**How to handle me correctly:**\n"
             "`!tag help` – Show tag commands help.(The helpfulness is debatable).\n"
             "`!bathtime` – Rinse off all that chat history for this channel. (Very cleansing).\n"
+            "`!ping` – Play some ping pong with some stats.\n"
             "Ping me to ask questions <:pls:1396080923572961361>\n"
             "#- There *may* be an Easter egg"
         ) 
